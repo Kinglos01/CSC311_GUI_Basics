31db56c06634a87e0fad7a6ff4f545fc4e63f153
Carlos Berio <carlos.a.berio@hotmail.com>
2025-02-14 14:23:30 -0500

    Finalized Commit

--- src/main/java/org/example/csc311_gui_basics/HelloApplication.java
+++ src/main/java/org/example/csc311_gui_basics/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 620, 380);
+        scene.getStylesheets().add(getClass().getResource("MyStyle.css").toExternalForm());
+        stage.setTitle("Wacky Buttons!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/csc311_gui_basics/HelloController.java
+++ src/main/java/org/example/csc311_gui_basics/HelloController.java
@@ -1,14 +1,58 @@
 package org.example.csc311_gui_basics;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Shape;
+
+
 public class HelloController {
+
+
+    @FXML
+    private ImageView Meme;
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button DogeButton;
+
+    @FXML
+    public Button PressMe;
+
+    @FXML
+    private Label WelcomeIn;
+
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        welcomeText.setText("Welcome to Wacky Buttons!!!");
     }
+
+    @FXML
+    protected void OnPressMeCLick(){
+        WelcomeIn.setText("That click was weird -_-");
+    }
+
+    boolean DogeAppear = false;
+
+    @FXML
+    protected void OnDontDoItClick() {
+
+        if (DogeAppear) {
+            Meme.setVisible(false);
+            DogeAppear = false;
+        }
+
+        else {
+            DogeAppear = true;
+            Meme.setVisible(true);
+        }
+    }
+
 }
